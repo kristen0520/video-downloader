ff1d51ff41c0dc1050a933a5bd56fee7da49818d
bunny <bunny@bunnys-MacBook-Pro.local>
2019-11-27 00:54:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const PORT = 5000;
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
+
+/*app.get('/', function (req, res) {
+   res.send('Youtube to mp4 downloader')
+})*/
+
+app.get(/^\/(.+)/, function(req, res){
+  let videoUrlQuery = req.query.v;
+  let videoUrl = req.params[0] + "?v=" + videoUrlQuery;
+    console.log("videoUrlQuery = " + videoUrlQuery );
+  const video = youtubedl(videoUrl,
+    // Optional arguments passed to youtube-dl.
+    ['--format=18'],
+    // Additional options can be given for calling `child_process.execFile()`.
+    { cwd: __dirname })
+    // Will be called when the download starts.
+    video.on('info', function(info) {
+      console.log('Download started')
+      console.log('filename: ' + info._filename)
+      console.log('size: ' + info.size)
+    })
+    video.pipe(fs.createWriteStream('myvideo.mp4'))
+})
+
+var server = app.listen(5000, function () {
+    console.log("Youtube downloader is running on port " + PORT);
+});
