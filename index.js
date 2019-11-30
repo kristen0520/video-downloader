f9cdd1151c964d84cc2fc823e63661dd06ac57b1
bunny <bunny@bunnys-mbp.attlocal.net>
2019-11-30 15:06:21 -0500

    runs on heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const fs = require('fs')
-const youtubedl = require('youtube-dl')
+const youtubedl = require('youtube-dl');
+let filename = "";
 
-app.get('/home', function (req, res) {
+app.get('/', function (req, res) {
   fs.readFile("index.html", function(err, data){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
@@ -12,38 +13,45 @@ app.get('/home', function (req, res) {
   });
 })
 
-app.get('/downloadComplete', function (req, res) {
-  fs.readFile("downloadComplete.html", function(err, data){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-})
-
-app.get(/^\/(.+)/, function(req, res){
+app.get('/download/:id*', function(req, res){
   let videoUrlQuery = req.query.v;
   let videoUrl = req.params[0] + "?v=" + videoUrlQuery;
     console.log("videoUrlQuery = " + videoUrlQuery );
+
   const video = youtubedl(videoUrl,
-    // Optional arguments passed to youtube-dl.
     ['--format=18'],
-    // Additional options can be given for calling `child_process.execFile()`.
     { cwd: __dirname })
-    // Will be called when the download starts.
-    let filename = videoUrlQuery+".mp4";
-    video.on('info', function(info) {
-      console.log('Download started')
-      console.log('filename: ' + info._filename)
-      console.log('size: ' + info.size)
-      filenameL = info._filename.length - 16;
-      filename = info._filename.substring(0, filenameL) + ".mp4";
 
-    video.pipe(fs.createWriteStream(filename))
-    res.redirect("/downloadComplete");
-      })
+  filename = videoUrlQuery+".mp4";
 
+  video.on('info', function(info) {
+    console.log('Download started')
+    console.log('filename: ' + info._filename)
+    console.log('size: ' + info.size)
+    filenameL = info._filename.length - 16;
+    filename = info._filename.substring(0, filenameL) + ".mp4";
+
+    video.pipe(fs.createWriteStream(filename));
+    })
+  video.on('end', function() {
+    file = __dirname + '/' + filename;
+    res.download(file, file, function (err) {
+     if (err) {
+       console.log(err)
+     } else {
+       console.log("deleting from server...")
+       setTimeout( function(){
+         fs.unlink(file,function(err){
+            if(err) return console.log(err);
+            console.log('file deleted successfully');
+          })
+        }
+        , 500);
+     }
+   })
+  })
 })
 
-var server = app.listen(5000, function () {
+var server = app.listen(PORT, function () {
     console.log("Youtube downloader is running on port " + PORT);
 });
